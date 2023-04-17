32603602b40758ad22d43e05c9dbbd8ff3a20102
anushkakakkar <91541334+anushkakakkar@users.noreply.github.com>
2023-04-17 20:49:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Todo from './Todo';
+import Todolist from './Todolist';
+
+//TODO LIST
+function App() {
+  return (
+    <div className="App">
+      {/*<Todo/> */}
+      <Todolist/>
+    </div>
+  );
+}
+
+export default App;
